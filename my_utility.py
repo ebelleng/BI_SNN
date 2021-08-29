f0ad3ada97de5723f7a1129dd7840f9ae281fce6
ebelleng <etienne.bellenger@gmail.com>
2021-08-29 19:23:41 -0400

    funciones Load-Save + updW + backward parcial

--- my_utility.py
+++ my_utility.py
@@ -5,11 +5,11 @@ import numpy  as np
 
 # Init.weights of the DL 
 def iniW(nodes_hidden, x, y):
-    _, len_x = x.shape # 375, 5
-    len_y, _ = y.shape # 1, 375
+    _, size_input = x.shape # 375, 5
+    size_output, _ = y.shape # 1, 375
     # Init. w1 y w2 
-    w1 = np.random.random((nodes_hidden, len_x)) #dim -> (20x5)
-    w2 = np.random.random((len_y, nodes_hidden)) #dim -> (1x20)
+    w1 = np.random.random((nodes_hidden, size_input)) #dim -> (20x5)
+    w2 = np.random.random((size_output, nodes_hidden)) #dim -> (1x20)
        
     return(w1,w2)
 
@@ -26,14 +26,39 @@ def forward(x,w1,w2):
     return a2.T    
 
 # STEP 2: Gradiente via BackPropagation
-def grad_bp(...):
-    #completar code    
-    return(...)    
+def backward(Act, y, w1,w2, mu):
+    # Calcular el error
+    cost = y - Act
+    # Calcular el gradiente oculto y salida    
+    dCdW = grad_bp(Act, w1, w2)
+    # Actualizar los pesos
+    w1, w2 = updW(w1, mu, dCdW)
+    
+    return w1, w2, cost 
+
+def grad_bp(Act, w1, w2):
+    a2 = Act.T
+    z2 = deriva_sigmoid(a2)
+    a1 = np.dot(z2,w2.T)
+    z1 = deriva_sigmoid(a1)
+    x = np.dot(z1, w1.T)
+
+    # Calcular gradiente capa oculta
+    dCdW1 = 0
+
+    # Calcular gradiente capa salida
+    dCdW2 = 0
+
+    return dCdW1, dCdW2   
 
 # Update SNN's Weight 
-def updW(...):    
-    #completar code    
-    return(...)
+def updW(w1, w2, mu=0.1, dCdW=(0,0)):    
+    dCdW1, dCdW2 = dCdW
+    # Actualizar pesos ocultos
+    w1 = w1 - mu * dCdW1   
+    # Actualizar pesos salida
+    w2 = w2 - mu * dCdW2
+    return w1, w2
 
 #Activation function
 def act_sigmoid(z):
@@ -70,12 +95,20 @@ def load_data(fname):
 
 #save weights of SNN in numpy format
 def save_w(w1,w2, fname_w, cost, fname_cost):    
-    #completar code    
-    return
+    # Guardar pesos 
+    np.savez_compressed(fname_w, w1, w2)
+    # Guardar mse
+    archivo = open(fname_cost, 'w')
+    archivo.write('Iteracion,MSE\n') # header
+    [ archivo.write(f'{i},{c}\n') for i, c in enumerate(cost, 1) ]
+    archivo.close()
     
 #load weight of SNN in numpy format
 def load_w(fname):
     #completar code
+    w1 = np.load(fname)['arr_0']
+    w2 = np.load(fname)['arr_1']
+
     return (w1,w2)      
 
 # function normalize
